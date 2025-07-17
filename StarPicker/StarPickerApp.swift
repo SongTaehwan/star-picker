6fa9692818ccd244648ca19c132f7b7f8e5607a2
송태환 <axoghks@gmail.com>
2025-07-17 16:08:12 +0900

    Initial Commit

---
+++ StarPicker/StarPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarPickerApp.swift
+//  StarPicker
+//
+//  Created by 송태환 on 7/17/25.
+//
+
+import SwiftUI
+
+@main
+struct StarPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
